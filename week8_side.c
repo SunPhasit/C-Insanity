e214667f849ee4f0e21dab6b3610fe7791f2c8c5
SunPhasit <42086629+SunPhasit@users.noreply.github.com>
2020-08-11 13:30:58 +0700

    Add files via upload

--- week8_side.c
+++ week8_side.c
@@ -1,14 +1,30 @@
 #include <stdio.h>
 int main()
 {
-    int w,h,c=0;
+    int w,h,c=0,i,j;
     scanf("%d %d",&w,&h);
-    int LCDs[w][h];
+    int LCDs[w+2][h+2], lines[h];
+    for(i=0;i<h+2;i++)
+        for(j=0;j<w+2;j++)
+            LCDs[i][j]=1;
     for(i=0;i<h;i++)
+    {
+        scanf("%d",&lines[i]);
         for(j=0;j<w;j++)
-            scanf("%d",&LCDs[i][j]);
-    for(i=0;i<h;i++)
-        for(j=0;j<w;j++)
+        {
+            LCDs[i+1][j+1]=lines[i]%10;
+            lines[i]/=10;
+            printf( "%d %d",lines[i],LCDs[i+1][j+1]);
+        }
+    }
+    for(i=0;i<h+2;i++)
+    {
+        for(j=0;j<w+2;j++)
+            printf("%d ",LCDs[i][j]);
+        printf("\n");
+    }
+    for(i=1;i<h+1;i++)
+        for(j=1;j<w+1;j++)
         {
               if(!LCDs[i][j])
               {
