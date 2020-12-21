8491dd22871c8992e2b8eda5d40351662668f5b0
SunPhasit <42086629+SunPhasit@users.noreply.github.com>
2020-12-21 16:20:18 +0700

    Update and rename week2_main.c to 2week2_main.c

--- 2week2_main.c
+++ 2week2_main.c
@@ -20,23 +20,54 @@ int findIntersection(int count1, int *elements1, int count2, int *elements2, int
 
 int findUnion(int count1, int *elements1, int count2, int *elements2, int *un) //Broken
 {
-    int i,j,k=0;
-    for(i=0;i<count1;i++)
+    int i=0,j=0,k=0;
+    /*for(i=0;i<count1;i++)
     {
         for(j=0;j<count2;j++)
         {
-            if(elements1[i]<=elements2[j])
+            if(elements1[i]<elements2[j])
+            {
+                un[k]=elements1[i];
+                k++;
+                j=count2;
+            }
+            else if(elements1[i]>elements2[j])
+            {
+                un[k]=elements2[j];
+                k++;
+            }
+        }
+    }*/
+    while((i<count1)&&(j<count2))
+    {
+        if(elements1[i]<elements2[j])
             {
                 un[k]=elements1[i];
                 k++;
                 i++;
             }
-            if(elements1[i]>elements2[j])
+        else if(elements1[i]>elements2[j])
             {
                 un[k]=elements2[j];
                 k++;
                 j++;
             }
+        else j++;
+    }
+    if (i<count1)
+    {
+        for(j=i;j<count1;j++)
+        {
+            k++;
+            un[k]=elements1[j];
+        }
+    }
+    else if (j<count2)
+    {
+        for(i=j;i<count2;i++)
+        {
+            k++;
+            un[k]=elements2[i];
         }
     }
     return k;
@@ -67,6 +98,7 @@ int main()
     }
     printf("\n");
     k=findUnion(n1,set1,n2,set2,un);
+    if(k==0) printf("Empty");
     for(i=0;i<k;i++)
     {
         printf("%d ",un[i]);
