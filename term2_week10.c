77aacab9f9e0d80c954f45a75f8d02a803cea819
SunPhasit <42086629+SunPhasit@users.noreply.github.com>
2021-02-22 16:14:13 +0700

    Create term2_week10.c

---
+++ term2_week10.c
@@ -0,0 +1,35 @@
+/*
+PPPPPPPPPPPPP     IIIIIII    NNN         NNN     KKK        KKK    YYY        YYY
+PPPPPPPPPPPPPP      III      NNNN        NNN     KKK      KKK       YYY      YYY
+PPP        PPPP     III      NNNNN       NNN     KKK    KKK          YYY    YYY
+PPP         PPP     III      NNN NN      NNN     KKK   KKK             YYY YYY
+PPP        PPPP     III      NNN  NN     NNN     KKKKKK                 YYYYY
+PPPPPPPPPPPPPP      III      NNN   NN    NNN     KKKKKK                  YYY
+PPPPPPPPPPPPP       III      NNN    NN   NNN     KKK  KKK                YYY
+PPP                 III      NNN     NN  NNN     KKK    KKK              YYY
+PPP                 III      NNN      NN NNN     KKK      KKK            YYY
+PPP                 III      NNN       NNNNN     KKK        KKK          YYY
+PPP                 III      NNN        NNNN     KKK         KKK         YYY
+PPP               IIIIIII    NNN         NNN     KKK          KKK        YYY
+*/
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/* == LINKED LISTS == */
+
+datatype struct node
+{
+    char name[LENGTH];
+    int age;
+    struct node *next;
+}data;
+
+int main()
+{
+    int i,n;
+    data *current=NULL, *head=NULL, *next=NULL
+    for(i=0;i<4;i++)
+    {
+        p=malloc(sizeof(data));
+    }
+}
